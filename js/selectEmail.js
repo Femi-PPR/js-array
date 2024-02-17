1349262f9eb8e22682839896a104c7f73e095849
Femi Oladapo <femioladapo3@gmail.com>
2024-02-17 20:57:27 +0000

    added email validation

--- js/selectEmail.js
+++ js/selectEmail.js
@@ -12,14 +12,16 @@ class Email {
     constructor(email) {
         this.email = email;
         this.id = ++Email.count;
-        this.$option = $(`<option value="${this.id}">${this.email}</option>`);
+        this.$option = $(
+            `<option value="${this.id}" class="overflow-hidden text-ellipsis max-w-full">${this.email}</option>`
+        );
         this.$selected = $(
-            `<small class="flex gap-1 items-center">${this.email} <i data-id="${this.id}" class="remove-email cursor-pointer text-red-600 icon-[mdi--remove-bold]"></i></small>`
+            `<small class="flex gap-1 max-w-28 items-center"><span class="overflow-hidden text-ellipsis">${this.email}</span> <i data-id="${this.id}" class="remove-email cursor-pointer text-red-600 hover:text-red-700 min-w-3 icon-[mdi--remove-bold]"></i></small>`
         );
         this.$item = $(`<div id="item-1" class="item bg-palette-2">
-    <button class="header w-full bg-palette-8 p-5 text-xl flex justify-between items-center">
-        <h2 class="overflow-hidden">${this.email}</h2>
-        <span class="icon-[ion--chevron-down] size-7"></span>
+    <button class="header w-full bg-palette-8 p-5 text-xl flex gap-3 justify-between items-center">
+        <h2 class="overflow-hidden text-ellipsis">${this.email}</h2>
+        <span class="icon-[ion--chevron-down] min-w-7 size-7"></span>
     </button>
     <div class="content transition-all flex gap-5 flex-wrap">
     </div>                    
@@ -71,41 +73,67 @@ function removeEmailfromSelection(emailObj) {
 
 $emailInputElem.on("input", () => {
     let disableSelect = $emailInputElem.val() !== "";
-    $emailSelectElem.attr("disabled", disableSelect);
+    $emailSelectElem.prop("disabled", disableSelect);
 });
+
+function emailValid(email) {
+    let input = document.getElementById("new-email");
+    let validity = input.validity;
+
+    if (Email.exists(email)) {
+        createAlert(
+            "error",
+            "The email address you provided already exists. Please use a different email address."
+        );
+        return false;
+    }
+
+    if (Email.count >= 30) {
+        createAlert(
+            "error",
+            "You have reached the maximum limit for email additions. No further emails can be added at this time."
+        );
+        return false;
+    }
+    if (validity.typeMismatch) {
+        createAlert(
+            "error",
+            "The email you provided is not valid. Please enter a valid email address."
+        );
+        return false;
+    }
+
+    return true;
+}
 
 $selectBtn.click(() => {
     if ($emailInputElem.val() === "" && $emailSelectElem.val() === null) {
-        createAlert("error", "THERE IS NOTHING HERE!!!!!!");
+        createAlert(
+            "error",
+            "No email address has been provided to be selected. Please enter an email address."
+        );
         return;
     }
 
     if ($emailInputElem.val() !== "") {
         let email = escapeHtml($emailInputElem.val());
-
-        if (Email.exists(email)) {
-            createAlert("error", "ALREADY EXISTS EMAIL!!!!!!");
-            return;
-        }
-
-        if (Email.count >= 30) {
-            createAlert("error", "TOO MANY EMAILS ALREADY. NO MORE!!!!!!");
-            return;
-        }
+        if (!emailValid(email)) return;
 
         addEmailtoSelection(new Email(email));
         $emailInputElem.val("");
-        $emailSelectElem.attr("disabled", false);
+        $emailSelectElem.prop("disabled", false);
     } else {
         let id = parseInt($emailSelectElem.val());
         let emailObj = Email.getEmailById(id);
         addEmailtoSelection(emailObj);
         emailObj.removeOption();
+        if ($emailSelectElem.children().length === 1) {
+            $("#default-selection").prop("selected", true);
+        }
     }
 });
-// Quiexercitationculpasuntcillumdolor.Loremdoloreconsecteturmagnaconsequatmagnaveniamnonadipisicingmagnamagnaenimnulla.Mollitdolaborumconsequatidsuntexercitationmagnacommododolornisiadnon.Iddolorlaboreoccaecatsuntconsequatadipisicingadsuntreprehenderitaliquip.Occaecatetidcommodoadidexcepteurnonvoluptate.Nonveniamdolorduisconsequatestenimipsumoccaecatad.
+// QuiexercitationculpasuntcillumdolorLoremdoloreconsecteturmagnaconsequatmagnaveniamnonadipisicingmagnamagnaenimnullaMollitdolaborumconsequatidsuntexercitationmagnacommododolornisiadnonIddolorlaboreoccaecatsuntconsequatadipisicingadsuntreprehenderitaliquipOccaecatetidcommodoadidexcepteurnonvoluptateNonveniamdolorduisconsequatestenimipsumoccaecatad@g
 $selectedDiv.on("click", ".remove-email", (event) => {
     const $target = $(event.target);
-    console.log($target.data("id"));
     removeEmailfromSelection(Email.getEmailById($target.data("id")));
 });
